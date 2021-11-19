12ac888a718921d092565ef0a489ec98372f33ca
unknown <julien.martinet523@gmail.com>
2021-11-18 20:23:47 -0500

    created scrape method

--- scrape.py
+++ scrape.py
@@ -1,25 +1,47 @@
 import praw
 import pandas as pd
 
-# Change the following dictionary to add/remove whatever information you want to scrape
-# Append the information to the correct column of the dictionary 
-# e.g. data['title'].append("My subreddit title")
+def scrape(subName):
+    
+    # create reddit instance
+    reddit = praw.Reddit(
+        client_id="pB9MUejeyq28DAbqhPTPdw",
+        client_secret="DjtWE0kvFBtO3jllv80JjcXbPs8MZg",
+        user_agent="dulien",
+    )
 
-# Enter the subreddit title that you are scraping
-subreddit = "subreddit"
+    # create dataframe
+    data = {
+        'TITLE': [],
+        'TEXT': [],
+        'REPLIES': [],
+        'SCORE': [],
+        'FLAIR': [],
+        'URL': []
+    }
 
-data = {
-    'title': [],
-    'replies': [],
-    'upvotes': [],
-    'downvotes': [],
-    'flair': []
-}
+    # create entries for MLMRecovery subreddit
+    submissions = reddit.subreddit(subName).new(limit=None)
 
-# Your code HERE!!!
+    # iterate through submissions and filter those containing "story"
+    for submission in submissions:
+        if "story" in submission.title.lower() or "story" in submission.selftext.lower() or submission.link_flair_text == "Story":
+            print(submission.title)
+            # add title, text, replies, score, flair, and url to dataframe
+            data['TITLE'].append(submission.title)
+            data['TEXT'].append(submission.selftext)
+            data['REPLIES'].append(submission.num_comments)
+            data['SCORE'].append(submission.score)
+            data['FLAIR'].append(submission.link_flair_text)
+            data['URL'].append(submission.url)
+        else:
+            print("Does not fit criteria")
+        
+    # convert data to csv file    
+    df = pd.DataFrame(data)
+    df.to_csv('{}.csv'.format(subName), index=False)
 
-
-df = pd.DataFrame(data)
-df.to_csv(subreddit + '.csv')
 
 #Run the script once for each subreddit you want to scrape
+scrape("MLMRecovery")
+scrape("antiMLM")
